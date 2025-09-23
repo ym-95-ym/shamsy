1eeaa915b7234ccd88a8970e57ed50e29a541b2d
ym-95-ym <y.mahayni94@gmail.com>
2025-09-23 23:47:22 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/shamsy"> 
+      <BrowserRouter> 
         <ScrollToTop />
         <div className="min-h-screen flex flex-col">
           <Navigation />
