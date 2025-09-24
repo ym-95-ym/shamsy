18c15ea4d02f47eff933ab264190dffbb86c6611
ym-95-ym <y.mahayni94@gmail.com>
2025-09-24 22:51:45 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-     base: '/shamsy/',
+     base: '/shamsy',
     },
   },
 }))
