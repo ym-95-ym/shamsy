b5a13a29a2306d9ad4610194cdaf441f42db2ec8
ym-95-ym <y.mahayni94@gmail.com>
2025-09-25 16:24:05 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ const AppContent: React.FC = () => {
   usePerformance();
   
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/shamsy">
       <SEOHead />
       <ScrollToTop />
       <div className="min-h-screen flex flex-col">
