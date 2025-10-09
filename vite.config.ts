c99f474af47bd84bde7688852e9ff8fb22c894f0
ym-95-ym <y.mahayni94@gmail.com>
2025-10-09 22:41:14 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 import { componentTagger } from "lovable-tagger"
 
 export default defineConfig(({ mode }) => ({
-    base: '/shamsy/', // <-- Korrekt hier!
+    base: '/', // <-- Korrekt hier!
   server: {
     host: "::",
     port: 8080,
