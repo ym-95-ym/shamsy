46b7fa63ac47620e83043035364c9fa3854f93f5
ym-95-ym <y.mahayni94@gmail.com>
2025-10-11 20:40:35 +0200

    Update SharedHome.tsx

--- src/components/pages/SharedHome.tsx
+++ src/components/pages/SharedHome.tsx
@@ -254,7 +254,7 @@ const SharedHome = () => {
           </div>
         </section>
       )}
-
+ 
       {/* Overview Section */}
       <section className="py-20 shamsy-bg-gradient">
         <div className="container mx-auto px-4">
